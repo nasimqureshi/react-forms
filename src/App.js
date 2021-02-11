b1f2d191e401e8a2909cedb4c19d5ce2e3e98c52
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-11 10:04:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return(
+    <>
+  <h1>Hello</h1>
+  <input type="text" placeholder="Enter Your Name" />
+  <button>Click me</button>
+  </>
+  )
+}
+
+
+export default App;
