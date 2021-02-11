7d06b16ec00786fdc4ec02f1dd5e893d20054e95
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-11 12:27:04 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React,{useState}  from 'react'
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  
+  const inputEvent = (event)=> {
+    console.log(event.target.value);
+    setName(event.target.value);
+    
+  }
   return(
     <>
-  <h1>Hello</h1>
-  <input type="text" placeholder="Enter Your Name" />
+  <h1>Hello {name} </h1>
+  <input type="text" placeholder="Enter Your Name"
+  onChange={inputEvent} />
   <button>Click me</button>
   </>
   )
 }
-
-
 export default App;
